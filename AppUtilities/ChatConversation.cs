50d27ecbeb9df7940ecf36c1028e52b61a6e0577
marcin.pierchala@icloud.com <marcin.pierchala@icloud.com>
2023-10-21 09:59:07 +0200

    createdNewProjToMaintenanceChat

---
+++ AppUtilities/ChatConversation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace AppUtilities
+{
+    public class ChatConversation
+    {
+        
+        public async Task<string> TalkWithChat(string query)
+        {
+            string theAnswer = "";
+            var opeiAi = new OpenAIAPI(AppData.ApiKey);
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = query;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+            completionRequest.MaxTokens = 1024;
+
+            var completions = await opeiAi.Completions.CreateCompletionAsync(completionRequest);
+
+            foreach (var completion in completions.Completions) 
+            {
+                theAnswer += completion.Text;
+            }
+
+            if(theAnswer.Length == 0)
+            {
+                theAnswer = "wystąpił błąd - to do exception handling";
+            }
+
+            return theAnswer;
+        }
+    }
+}
