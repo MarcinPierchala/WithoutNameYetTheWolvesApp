052a1221f4915d97f9fe1d2f3e235d5e9afd4462
marcin.pierchala@icloud.com <marcin.pierchala@icloud.com>
2023-10-23 17:34:53 +0200

    beforeAddAPIToken

---
+++ WithoutNameYetTheWolvesApp/Models/ResponseModel.cs
@@ -0,0 +1,19 @@
+﻿using OpenAI_API.Completions;
+
+namespace WithoutNameYetTheWolvesApp.Models
+{
+    public class ResponseModel
+    {
+        public string id { get; set; }
+        public int created {  get; set; }
+        public string model { get; set; }
+        public Choice[] choices { get; set; }
+
+    }
+
+    public class Choice
+    {
+        public string text { get; set; }
+        public int index { get; set; }
+    }
+}
---
+++ WithoutNameYetTheWolvesApp/Models/SearchModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WithoutNameYetTheWolvesApp.Models
+{
+    public class SearchModel
+    {
+        [Required]
+        public string Prompt { get; set; }
+    }
+}
